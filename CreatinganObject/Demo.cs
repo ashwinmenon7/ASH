df7ba7a693908cd1f7719dcbbdf9cbb8bccc55c4
LAPTOP-H4R65UHV\Ashwin Menon <ashwinmenon7@gmail.com>
2021-05-08 17:02:03 +0100

    Object

---
+++ CreatinganObject/Demo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatinganObject
+{
+   public class Demo
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+
+            s.fun1();
+        }
+    }
+}
---
+++ CreatinganObject/Sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatinganObject
+{
+    public class Sample
+    {
+      private int a = 10;
+
+      private string s = "Ash";
+
+        public void fun1()
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+
+        }
+
+    }
+}
