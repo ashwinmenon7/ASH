bb0cf40c13e828f54281c3ca136b5e49542c6156
LAPTOP-H4R65UHV\Ashwin Menon <ashwinmenon7@gmail.com>
2021-05-08 16:55:31 +0100

    Excecuted Arraylist

---
+++ Arraylist/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylist
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            ArrayList w = new ArrayList();
+
+            w.Add("Ashwin");
+            w.Add("Menon");
+            w.Add(26);
+            w.Add(true);
+            w.Add(32.11);
+
+            foreach(var i in w)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
